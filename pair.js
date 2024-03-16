6ba51a04df9e3fc5a2a242ad9ee863e21844624b
King Ibrahim <162009674+ibrahimaitech@users.noreply.github.com>
2024-03-16 21:34:34 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-    default: France_King,
+    default: IBRAHIM_TECH,
     useMultiFileAuthState,
     delay,
     makeCacheableSignalKeyStore,
@@ -20,13 +20,13 @@ function removeFile(FilePath){
 router.get('/', async (req, res) => {
     const id = makeid();
     let num = req.query.number;
-        async function FLASH_MD_PAIR_CODE() {
+        async function IBRAHIM_TECH_PAIR_CODE() {
         const {
             state,
             saveCreds
         } = await useMultiFileAuthState('./temp/'+id)
      try {
-            let Pair_Code_By_France_King = France_King({
+            let Pair_Code_By_IBRAHIM_TECH = IBRAHIM_TECH({
                 auth: {
                     creds: state.creds,
                     keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
@@ -35,16 +35,16 @@ router.get('/', async (req, res) => {
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
                 browser: ["Chrome (Linux)", "", ""]
              });
-             if(!Pair_Code_By_France_King.authState.creds.registered) {
+             if(!Pair_Code_By_IBRAHIM_TECHK.authState.creds.registered) {
                 await delay(1500);
                         num = num.replace(/[^0-9]/g,'');
-                            const code = await Pair_Code_By_France_King.requestPairingCode(num)
+                            const code = await Pair_Code_By_IBRAHIM_TECH.requestPairingCode(num)
                  if(!res.headersSent){
                  await res.send({code});
                      }
                  }
-            Pair_Code_By_France_King.ev.on('creds.update', saveCreds)
-            Pair_Code_By_France_King.ev.on("connection.update", async (s) => {
+            Pair_Code_By_IBRAHIM_TECH.ev.on('creds.update', saveCreds)
+            Pair_Code_By_IBRAHIM_TECH.ev.on("connection.update", async (s) => {
                 const {
                     connection,
                     lastDisconnect
@@ -54,35 +54,35 @@ router.get('/', async (req, res) => {
                 let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                 await delay(800);
                let b64data = Buffer.from(data).toString('base64');
-               let session = await Pair_Code_By_France_King.sendMessage(Pair_Code_By_France_King.user.id, { text: '' + b64data });
+               let session = await Pair_Code_By_IBRAHIM_TECH.sendMessage(Pair_Code_By_IBRAHIM_TECH.user.id, { text: '' + b64data });
 
-               let FLASH_MD_TEXT = `
+               let IBRAHIM_TECH_TEXT = `
 *_Pair Code for IBRAHIM-TECH Connected_*
-*_Made With 🔱_*
+*_Made With King ibrahim🔱_*
 *By IBRAHIM ADAMS*
 ______________________________________
 ╔════◇
-║ *『 THANKS FOR CHOOSING IBRAHIM-TECH 』*
-║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
+║ *『  IBRAHIM-TECH OFFICIAL 』*
+║ YOU HAVE SUCCESSFULLY SELECTED THE BEST BOT
 ╰═════════════⊷
 ╔═════◇
-║  『••• FOR HELP VISIT•••』
-║❒
-║❒ *Owner:* _https://wa.me/25471077266_
-║❒
+║  『••• CONTACT THE OWNER 👇👇•••』
+║❒ *Name:* IBRAHIM ADAMS
+║❒ *Owner:* https://wa.me/message/74F2PC4JA4F3P1
+║❒ *Group:* https://chat.whatsapp.com/L7T4iMvnfwCA8oLkUIRCFj
 ╰═════════════⊷ 
 _____________________________________
 
 _Don't Forget To Give a Star To My Repo_`
- await Pair_Code_By_France_King.sendMessage(Pair_Code_By_Ibrahim_Tech.user.id,{text:IBRAHIM_TECH_TEXT},{quoted:session})
+ await Pair_Code_By_Ibrahim_Tech.sendMessage(Pair_Code_By_Ibrahim_Tech.user.id,{text:IBRAHIM_TECH_TEXT},{quoted:session})
  
 
         await delay(100);
-        await Pair_Code_By_France_King.ws.close();
+        await Pair_Code_By_Ibrahim_Tech.ws.close();
         return await removeFile('./temp/'+id);
             } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
-                    FLASH_MD_PAIR_CODE();
+                    IBRAHIM_TECH_PAIR_CODE();
                 }
             });
         } catch (err) {
