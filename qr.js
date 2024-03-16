2073c3eedcaff62e0f45d2bf2dde9de0d2e8a2ff
King Ibrahim <162009674+ibrahimaitech@users.noreply.github.com>
2024-03-16 21:42:21 +0300

    IBRAHIM_TECH

--- qr.js
+++ qr.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-	default: France_King,
+	default: IBRAHIM_TECH,
 	useMultiFileAuthState,
 	jidNormalizedUser,
 	Browsers,
@@ -28,13 +28,13 @@ const {
 } = require("node:fs/promises")
 router.get('/', async (req, res) => {
 	const id = makeid();
-	async function FLASH_MD_QR_CODE() {
+	async function IBRAHIM_TECH_QR_CODE() {
 		const {
 			state,
 			saveCreds
 		} = await useMultiFileAuthState('./temp/' + id)
 		try {
-			let Qr_Code_By_France_King = France_King({
+			let Qr_Code_By_IBRAHIM_TECH = IBRAHIM_TECH({
 				auth: state,
 				printQRInTerminal: false,
 				logger: pino({
@@ -43,8 +43,8 @@ router.get('/', async (req, res) => {
 				browser: Browsers.macOS("Desktop"),
 			});
 
-			Qr_Code_By_France_King.ev.on('creds.update', saveCreds)
-			Qr_Code_By_France_Kingr.ev.on("connection.update", async (s) => {
+			Qr_Code_By_IBRAHIM_TECH.ev.on('creds.update', saveCreds)
+			Qr_Code_By_IBRAHIM_TECH.ev.on("connection.update", async (s) => {
 				const {
 					connection,
 					lastDisconnect,
@@ -56,33 +56,31 @@ router.get('/', async (req, res) => {
 					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
 					await delay(800);
 				   let b64data = Buffer.from(data).toString('base64');
-				   let session = await Qr_Code_By_France_King.sendMessage(Qr_Code_By_France_King.user.id, { text: '' + b64data });
+				   let session = await Qr_Code_By_IBRAHIM_TECH.sendMessage(Qr_Code_By_IBRAHIM_TECH.user.id, { text: '' + b64data });
 	
-				   let FLASH_MD_TEXT = `
-*_Qr Code By France King_*
+				   let IBRAHIM_TECH_TEXT = `
+*_Qr Code By Ibrahim King_*
 *_Made With 🤍_*
 ______________________________________
 ╔════◇
-║ *『 WOW YOU CHOOSEN FLASH-MD 』*
-║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
-╚══════════════════════╝
+║ *『  IBRAHIM-TECH OFFICIAL 』*
+║ YOU HAVE SUCCESSFULLY SELECTED THE BEST BOT
+╰═════════════⊷
 ╔═════◇
-║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ *Instagram:* _https://instagram.com/france.king1_
-║❒ *Owner:* _https://wa.me/254757835036_
-║❒ *Repo:* _https://github.com/franceking1/Flash-Md_
-║❒ *WaGroup:* _https://chat.whatsapp.com/CtBGmUkwXiA6kGYAyU5MaX_
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaTbb3p84Om9LRX1jg0P_
-╚══════════════════════╝ 
+║  『••• CONTACT THE OWNER 👇👇•••』
+║❒ *Name:* IBRAHIM ADAMS
+║❒ *Owner:* https://wa.me/message/74F2PC4JA4F3P1
+║❒ *Group:* https://chat.whatsapp.com/L7T4iMvnfwCA8oLkUIRCFj
+╰═════════════⊷
 _____________________________________
 	
 _Don't Forget To Give Star To My Repo_`
-	 await Qr_Code_By_France_King.sendMessage(Qr_Code_By_France_King.user.id,{text:FLASH_MD_TEXT},{quoted:session})
+	 await Qr_Code_By_IBRAHIM_TECH.sendMessage(Qr_Code_By_IBRAHIM_TECH.user.id,{text:IBRAHIM_TECH_TEXT},{quoted:session})
 
 
 
 					await delay(100);
-					await Qr_Code_By_France_King.ws.close();
+					await Qr_Code_By_IBRAHIM_TECH.ws.close();
 					return await removeFile("temp/" + id);
 				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
 					await delay(10000);
@@ -99,6 +97,6 @@ _Don't Forget To Give Star To My Repo_`
 			await removeFile("temp/" + id);
 		}
 	}
-	return await FLASH_MD_QR_CODE()
+	return await IBRAHIM_TECH_QR_CODE()
 });
 module.exports = router
